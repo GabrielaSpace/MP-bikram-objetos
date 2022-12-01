86d5c1bc67454a2a128c0eee337bd285e01bcda7
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2022-12-01 12:00:16 +0100

    hasta la 17

--- index.js
+++ index.js
@@ -28,8 +28,29 @@ const RGB = [Led.Rojo, Led.Verde, Led.Azul];
 // Modificación de propiedades
 //  9.- Dado un objeto de nombre Portatil modifica el valor de la propiedad modelo por el valor P345
 Portatil.modelo = 'P345';
-
 //  10.- Dado un objeto de nombre Concierto añade el valor Guns N' Roses a la propiedad cartelera
 Concierto.cartelera.push("Guns N' Roses");
 //  11.- Dado un objeto de nombre Concierto modifica el valor de la propiedad fecha por el valor new Date() (fecha de hoy)
+Concierto.fecha = new Date();
 //  12.- Dado un objeto de nombre Impresora modifica el valor de la propiedad imprimiendo por el valor objeto con propiedades: nombreArchivo, copias, numPaginas
+Impresora.imprimiendo = {  nombreArchivo:'holi.doc', copias:2, numPaginas:3 };
+
+
+// Objetos
+// Declaración
+//  13.- Crea un objeto de nombre Noticia que tenga las propiedades: titular, cuerpo
+const Noticia = {titular:'xxx', cuerpo:'yyyy'};
+//  14.- Crea un objeto de nombre Persona que tenga las propiedades: nombre, apellidos, edad
+const Persona = {nombre:'david', apellidos:'Lima Garcia', edad:28 };
+//  15.- Crea un objeto de nombre Avion que tenga las propiedades: numPasajeros, función despegar (imprime por consola 'despegando'), función volar (imprime por consola llegando al destino), función aterrizar (imprime por consola 'aterrizando'
+const Avion={ numPasajeros:22 , 
+    despegar: function  (){console.log('despegando');}, 
+    volar : function(){ console.log('llegando al destino');},
+    aterrizar: function (){console.log('aterrizando');}
+};
+//  16.- Crea un objeto de nombre Paquete que tenga las propiedades: array contenido con todos los objetos que contenga el paquete
+const Paquete = { contenido:['comida','bebida']};
+//  17.- Crea un objeto de nombre Pais que tenga las propiedades: numHabitantes, continente, gentilicio
+const Pais ={numHabitantes:250000, continente:'Europeo', gentilicio:'español'};
+
+
